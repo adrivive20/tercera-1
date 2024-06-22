76e7fe2d0a8f31e8c7d62cf4fe28b264f9b0c40f
Cesar Segura <info@ticoworks.com>
2024-06-22 16:54:07 -0600

    primera clase

---
+++ parte_superior.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="English">
+
+</html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="proyecto de tercera 1" content="pagina de prueba">
+    <title>
+        Tercera 1
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu_izquierda">
+    <a href="index.php">
+        <img src="./images/menu.png"alt="">
+    </a>
+        <p class="texto-menu-izquierda">
+            mariposa
+        </p>
+    </div>
+</menu>
